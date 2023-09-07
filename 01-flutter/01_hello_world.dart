a73baa3ed78f559670ff6b3e7bd668fabf1d1b78
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2023-09-07 18:00:43 -0400

    Fin hola mundo

---
+++ 01-flutter/01_hello_world.dart
@@ -0,0 +1,15 @@
+void main() {
+  final String pokemon = 'Dito';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor'];
+  final  sprites = <String>['ditto/front.png'];
+
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+""");
+}
